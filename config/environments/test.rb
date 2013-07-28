f0e44d3536bd7648eccdf5ddc4ec72743839be1f
Rex Feng <rexfeng@gmail.com>
2013-07-28 00:44:14 -0400

    add devise log in

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ FoursquareMapped::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # devise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
